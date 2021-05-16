4a3fb897df3db17e5dabb194dadb6de245e556f8
omarabdiwali <63807504+omarabdiwali@users.noreply.github.com>
2021-05-16 01:23:55 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import ButtonAppBar from './components/ButtonAppBar';
 import MovieData from './components/movieData';
 import TVData from './components/tvData';
